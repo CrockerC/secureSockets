d6a76b3c8bebc7fa395df7bcf1dbe4c5fd3c73e5
Christopher Crocker <66031632+CrockerC@users.noreply.github.com>
2020-10-06 22:44:06 -0400

    removed need to reference self.sock

--- secureSockets.py
+++ secureSockets.py
@@ -1,4 +1,6 @@
+
 import socket
+from _socket import getdefaulttimeout
 from RSAenc import *
 from AES import *
 from typing import Any, Tuple, Union
@@ -6,67 +8,63 @@ from typing import Any, Tuple, Union
 _Address = Union[tuple, str]
 _RetAddress = Any
 
-__keySize = 3072
-
-lock = threading.Lock()
-
 # todo, make this save the key to a file to save time so that a new key doesnt have to be generated each time
-PRIVRSA, PUBRSA = rsaGenerator(__keySize).generate()
+PRIVRSA, PUBRSA = rsaGenerator(3072).generate()
 
 
 # todo, add udp support
 # todo, add more socket functions
 # todo, figure out how to make accepted socks secureSockets instead of having them be secureSockets containing normal sockets
 # todo, maybe not make the secureSocket inherit the socket class, just contain one?
+# todo, maybe put some sort of calling filter in to just refer to self.sock when the self is called when appropriate?
 
 class secureSocket(socket.socket):
-    def __init__(self, *args):
-        super(secureSocket, self).__init__()
+    def __init__(self, family=-1, type=-1, proto=-1, fileno=None):
+        socket.socket.__init__(self, family=family, type=type, proto=proto, fileno=fileno)
         self.pubRSAKey = None
         self.secKey = None
-        self.sock = self
+        self.sock = None
 
     def connect_sec(self, address: Union[_Address, bytes]) -> None:
         self.connect(address)
-        self.__sendRSA()
-        self.pubRSAKey = self.__getRSA()
-        self.secKey = self.__genSendSecKey()
+        self._sendRSA()
+        self.pubRSAKey = self._getRSA()
+        self.secKey = self._genSendSecKey()
 
     def connect_ex_sec(self, address: Union[_Address, bytes]) -> int:
         suc = self.connect_ex(address)
         if suc:
             return suc
-        self.__sendRSA()
-        self.pubRSAKey = self.__getRSA()
-        self.secKey = self.__genSendSecKey()
+        self._sendRSA()
+        self.pubRSAKey = self._getRSA()
+        self.secKey = self._genSendSecKey()
         return suc
 
     def accept_sec(self) -> Tuple[socket.socket, _RetAddress]:
         sock, addr = self.accept()
-        nSock = secureSocket(sock)
-        nSock.sock = sock
-
-        nSock.pubRSAKey = nSock.__getRSA()
-        nSock.__sendRSA()
-        nSock.secKey = nSock.__getSecKey()
-        return nSock, addr
+        sSock = secureSocket(sock.family, sock.type, sock.proto, fileno=sock.fileno())
+        sSock.pubRSAKey = sSock._getRSA()
+        sSock._sendRSA()
+        sSock.secKey = sSock._getSecKey()
+        sSock.sock = sock
+        return sSock, addr
 
     def sendall_sec(self, data: bytes, flags: int = 0) -> None:
         encrypted = bytes(encryptAES(data, self.secKey), 'utf-8')
-        self.sock.sendall(self.__addLen(encrypted), flags)
+        self.sendall(self._addLen(encrypted), flags)
 
     def recv_sec(self, timeout: int = None, st: bool = False, flags: int = 0) -> bytes:
-        data = self.__recv_data(timeout, st, flags)
+        data = self._recv_data(timeout, st, flags)
         return decryptAES(data, self.secKey)
 
-    def __recv_data(self, timeout=None, st=False, flags: int = 0):
+    def _recv_data(self, timeout=None, st=False, flags: int = 0):
         if timeout is not None:
             self.settimeout(timeout)
-        lenData = self.__recvall(4, flags)
+        lenData = self._recvall(4, flags)
         if not lenData:
             return False
         lenData = struct.unpack('>I', lenData)[0]
-        data = bytes(self.__recvall(lenData, flags))
+        data = bytes(self._recvall(lenData, flags))
         if timeout is not None:
             self.settimeout(None)
         if st:
@@ -74,27 +72,27 @@ class secureSocket(socket.socket):
         else:
             return data
 
-    def __recvall(self, n, flags):
+    def _recvall(self, n, flags):
         data = bytearray()
         while len(data) < n:
-            packet = self.sock.recv(n - len(data), flags)
+            packet = self.recv(n - len(data), flags)
             if not packet:
                 return False
             data.extend(packet)
         return data
 
     @staticmethod
-    def __addLen(data):
+    def _addLen(data):
         return struct.pack('>I', len(data)) + data
 
-    def __sendRSA(self):
+    def _sendRSA(self):
         if self.fileno() != -1:
-            self.sock.sendall(self.__addLen(b'\x00' + PUBRSA.export_key()))
+            self.sendall(self._addLen(b'\x00' + PUBRSA.export_key()))
             return True
         return False
 
-    def __getRSA(self):
-        data = self.__recv_data(3)
+    def _getRSA(self):
+        data = self._recv_data(3)
         if not data:
             return False
         if data[0:1] == b'\x00':
@@ -103,40 +101,40 @@ class secureSocket(socket.socket):
         else:
             return False
 
-    def __genSendSecKey(self):
+    def _genSendSecKey(self):
         key = genAESKey()
         data = encryptRSAsingle(key, self.pubRSAKey)
-        self.sock.sendall(self.__addLen(b'\x01' + data))
+        self.sendall(self._addLen(b'\x01' + data))
         return key
 
-    def __getSecKey(self):
-        secKey = self.__recv_data(3)
+    def _getSecKey(self):
+        secKey = self._recv_data(3)
         if secKey is False:
             return False
         if secKey[0:1] == b'\x01':
             return decryptRSAsingle(secKey[1:], PRIVRSA)
 
 
-def __test1(sock):
+def _test1(sock):
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind(('localhost', 10000))
     sock.listen(1)
     sock1, addr = sock.accept_sec()
 
     data = str(sock1.recv_sec(), 'utf-8')
-    print(data)
+    print(data, sock1)
 
     sock1.sendall_sec(bytes("Success 2!", 'utf-8'))
 
 
-def __test2(sock):
+def _test2(sock):
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.connect_sec(('localhost', 10000))
 
     sock.sendall_sec(bytes("Success 1!", 'utf-8'))
 
     data = str(sock.recv_sec(), 'utf-8')
-    print(data)
+    print(data, sock)
 
 
 if __name__ == "__main__":
@@ -145,5 +143,5 @@ if __name__ == "__main__":
     s1 = secureSocket(socket.AF_INET, socket.SOCK_STREAM)
     s2 = secureSocket(socket.AF_INET, socket.SOCK_STREAM)
 
-    threading.Thread(target=__test1, args=(s1,)).start()
-    threading.Thread(target=__test2, args=(s2,)).start()
+    threading.Thread(target=_test1, args=(s1,)).start()
+    threading.Thread(target=_test2, args=(s2,)).start()
